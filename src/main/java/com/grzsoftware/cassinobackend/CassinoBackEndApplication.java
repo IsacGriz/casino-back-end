d4ae524d4f85e8c2dc687fdce7f1dc442717a8fa
isacgriz <isac.grizante@jala.university>
2024-05-24 00:12:28 -0300

    first commit

---
+++ src/main/java/com/grzsoftware/cassinobackend/CassinoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.grzsoftware.cassinobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassinoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassinoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grzsoftware/cassinobackend/CassinoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grzsoftware.cassinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassinoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
